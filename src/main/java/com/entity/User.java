d2309c2763bb2be42806036ab962bf9832adc039
tushar <tusharhake60@gmail.com>
2026-05-21 22:54:19 +0530

    api changes

--- src/main/java/com/entity/User.java
+++ src/main/java/com/entity/User.java
@@ -6,12 +6,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
+@Table(name = "user")
 public class User {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int userid;
-	
+
 	String firstname;
 	String lastname;
 	String email;
@@ -22,76 +23,97 @@ public class User {
 	String role;
 	String gender;
 	int empid;
+
 	public User() {
 		super();
 	}
+
 	public int getUserid() {
 		return userid;
 	}
+
 	public void setUserid(int userid) {
 		this.userid = userid;
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public long getContactno() {
 		return contactno;
 	}
+
 	public void setContactno(long contactno) {
 		this.contactno = contactno;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getConfirmpassword() {
 		return confirmpassword;
 	}
+
 	public void setConfirmpassword(String confirmpassword) {
 		this.confirmpassword = confirmpassword;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public int getEmpid() {
 		return empid;
 	}
+
 	public void setEmpid(int empid) {
 		this.empid = empid;
 	}
-	
-	
 
 }
