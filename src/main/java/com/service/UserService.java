9b7b28d11715bf9d4dc2f09a3e8e5f98a455c82a
tushar <tusharhake60@gmail.com>
2026-07-07 19:58:25 +0530

    emp id changes

--- src/main/java/com/service/UserService.java
+++ src/main/java/com/service/UserService.java
@@ -9,47 +9,30 @@ import com.repository.UserRepository;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	UserRepository repository;
-	
-	//Registration user
-	public String regidtrstion(User u)
-	{
-		User exist=repository.findByUsername(u.getUsername());
-		User empid=repository.findByEmpid(u.getEmpid());
-		
-		if(empid!=null)
-		{
-			return "Alredy rejisted this employee";
-		}
-		else if(exist==null)
-		{
+
+	// Registration user
+	public String regidtrstion(User u) {
+		User exist = repository.findByUsername(u.getUsername());
+		if (exist == null) {
 			repository.save(u);
 			return "User regidtration succefully";
-		}
-		else
-		{
+		} else {
 			return "Username alredy existed. use anothe username";
 		}
 	}
-	
-	//Login
-	public User login(UserDTO dto)
-	{
-		User exist=repository.findByUsername(dto.getUsername());
-		if(exist==null)
-		{
+
+	// Login
+	public User login(UserDTO dto) {
+		User exist = repository.findByUsername(dto.getUsername());
+		if (exist == null) {
 			return null;
-		}
-		else
-		{
-			if(exist.getPassword().equals(dto.getPassword()))
-			{
+		} else {
+			if (exist.getPassword().equals(dto.getPassword())) {
 				return exist;
-			}
-			else
-			{
+			} else {
 				return null;
 			}
 		}
